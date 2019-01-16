852d0a6907b57fef195c43e527b22e7e0ae3a170
devefx <devefx@163.com>
2019-01-16 17:10:08 +0800

    update install script

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -3,7 +3,11 @@
 # exit this script if any commmand fails
 set -e
 
-python install_cocos_x.py
-
+# install cocos engin
+if [ ! -d "./cocos2d-x-3.17.1" ]; then
+    echo "install cocos engin..."
+    wget https://digitalocean.cocos2d-x.org/Cocos2D-X/cocos2d-x-3.17.1.zip
+    unzip cocos2d-x-3.17.1.zip
+fi
 
 echo "before-install.sh execution finished!"
