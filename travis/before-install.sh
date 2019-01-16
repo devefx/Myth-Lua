69ce60b634fd3e351f5ffa6e684ac6276fceaf5c
devefx <devefx@163.com>
2019-01-16 18:49:11 +0800

    fix a bug

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -6,7 +6,7 @@ set -e
 WORK_DIR=$(cd "$(dirname "$0")"; pwd)
 
 # install cocos engin
-if [ ! -d "$HOME/cocos2d-x-3.17.1/templates/cocos2dx_files.json" ]; then
+if [ ! -f "$HOME/cocos2d-x-3.17.1/templates/cocos2dx_files.json" ]; then
     echo "install cocos engin..."
     wget https://digitalocean.cocos2d-x.org/Cocos2D-X/cocos2d-x-3.17.1.zip
     rm -rf $HOME/cocos2d-x-3.17.1
